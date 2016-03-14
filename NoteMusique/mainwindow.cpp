7993ee9a038b81f682c1ce28f5c9d185cb5f1914
kirapoetica974 <retco.contact@gmail.com>
2016-03-14 15:32:42 +0100

    ajout des notes de musique

--- NoteMusique/mainwindow.cpp
+++ NoteMusique/mainwindow.cpp
@@ -6,25 +6,31 @@
 #include <QSize>
 #include <QDesktopWidget>
 #include <QDebug>
+#include <QStackedWidget>
 
 
 
 MainWindow::MainWindow()
 {
 
-    resize(500,500);
+    // Redimension de la fenetre d'origine
+    resize(700,500);
+
+
     // Largeur de la fenetre de l'application
     int largeur = size().width();
 
     // hauteur de la fenetre de l'application
     int hauteur = size().height();
 
+
+    // Création du bouton retour
     retour = new QPushButton("Retour",this);
     retour->setGeometry((largeur*10)/100,hauteur-((hauteur*20)/100),(largeur*20)/100,(hauteur*10)/100);//positionH, positionV, tailleH, tailleV
     //retour->deleteLater();
     retour->show();
 
-    stack = new QStackedWidget(this);
+    /*stack = new QStackedWidget(this);
     fen1 = new FenetreSecondaire1();
 
     stack->addWidget(fen1);
@@ -32,8 +38,46 @@ MainWindow::MainWindow()
     this->setCentralWidget(stack);
     statck->setCurrentIndex(0); // on affiche la première fenêtre à l'ouverture du programme
 
-    connect(fen1, SIGNAL(askDisplayFen(int)), this, SLOT(slotDisplayFen(int)));
+    connect(fen1, SIGNAL(askDisplayFen(int)), this, SLOT(slotDisplayFen(int)));*/
 
+
+    // Insertion du clavier sur la fenetre
+    do1 = new QPushButton("do",this);
+    re1 = new QPushButton("re",this);
+    mi1 = new QPushButton("mi",this);
+    fa1 = new QPushButton("fa",this);
+    sol1 = new QPushButton("sol",this);
+    la1 = new QPushButton("la",this);
+    si1 = new QPushButton("si",this);
+    do2 = new QPushButton("do",this);
+    re2 = new QPushButton("re",this);
+    mi2 = new QPushButton("mi",this);
+    fa2 = new QPushButton("fa",this);
+    sol2 = new QPushButton("sol",this);
+    la2 = new QPushButton("la",this);
+    si2 = new QPushButton("si",this);
+
+
+    // Modification de la forme et du placement des boutons.
+    int largeurDeTouche = (largeur*5)/100;
+    int hauteurDeTouche = (hauteur*30)/100;
+    int xTouche = (largeur*10)/100;
+    int yTouche = hauteur/3;
+
+    do1->setGeometry(xTouche, yTouche, largeurDeTouche, hauteurDeTouche);
+    re1->setGeometry(xTouche+largeurDeTouche, yTouche, largeurDeTouche, hauteurDeTouche);
+    mi1->setGeometry(xTouche+(2*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    fa1->setGeometry(xTouche+(3*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    sol1->setGeometry(xTouche+(4*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    la1->setGeometry(xTouche+(5*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    si1->setGeometry(xTouche+(6*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    do2->setGeometry(xTouche+(7*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    re2->setGeometry(xTouche+(8*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    mi2->setGeometry(xTouche+(9*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    fa2->setGeometry(xTouche+(10*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    sol2->setGeometry(xTouche+(11*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    la2->setGeometry(xTouche+(12*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
+    si2->setGeometry(xTouche+(13*largeurDeTouche), yTouche, largeurDeTouche, hauteurDeTouche);
 }
 
 MainWindow::~MainWindow()
--- NoteMusique/mainwindow.h
+++ NoteMusique/mainwindow.h
@@ -20,6 +20,21 @@ public:
     ~MainWindow();
     QPushButton* retour;
 
+    QPushButton* do1;
+    QPushButton* re1;
+    QPushButton* mi1;
+    QPushButton* fa1;
+    QPushButton* sol1;
+    QPushButton* la1;
+    QPushButton* si1;
+    QPushButton* do2;
+    QPushButton* re2;
+    QPushButton* mi2;
+    QPushButton* fa2;
+    QPushButton* sol2;
+    QPushButton* la2;
+    QPushButton* si2;
+
 public slots:
     void slotDisplayFen(int fenIndex);
 
